8ee0725658436836a15bc4248704abf09bbafcc4
Artem Pstygo <apstygo.dev@gmail.com>
2023-05-14 01:57:10 +0400

    implement proper hp calculation and inputs

--- ai/src/third_strike_ai/envs/third_strike.py
+++ ai/src/third_strike_ai/envs/third_strike.py
@@ -6,7 +6,7 @@ from typing import Any
 
 import numpy as np
 import gymnasium as gym
-from PIL import Image
+from PIL import Image, ImageOps
 from third_strike_ai import constants as const
 
 
@@ -18,8 +18,14 @@ class Connection:
 class ThirdStrikeEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"]}
 
-    def __init__(self, executable: str, render_mode: str | None = None):
+    def __init__(
+        self, 
+        executable: str,
+        is_player_one: bool = True, 
+        render_mode: str | None = None
+    ):
         self.executable = executable
+        self.is_player_one = is_player_one
         self.connection: Connection | None = None 
 
         # Observations are frames
@@ -37,7 +43,23 @@ class ThirdStrikeEnv(gym.Env):
         self.render_mode = render_mode
 
     def step(self, action):
-        return super().step(action)
+        if self.connection is None:
+            raise RuntimeError('Cannot step while not connected to emulator.')
+
+        # send inputs
+        inputs = self._action_to_inputs(action)
+        self.connection.socket.send(bytearray(inputs))
+
+        # get state
+        observation, info = self._get_state()
+
+        # calculate reward and stop conditions
+        # TODO: do proper calculations
+        reward = 0
+        terminated = False
+        truncated = False
+
+        return observation, reward, terminated, truncated, info
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
@@ -59,12 +81,8 @@ class ThirdStrikeEnv(gym.Env):
         # store connection
         self.connection = Connection(process, sock)
 
-        # receive observation
-        frame = self._receive_frame(self.connection)
-        observation = np.asarray(frame)
-        info = self._get_info(frame)
-
-        return (observation, info)
+        # get and return state
+        return self._get_state()
 
     def render(self):
         # TODO: Implement
@@ -73,15 +91,58 @@ class ThirdStrikeEnv(gym.Env):
     def close(self):
         self._close_connection()
 
+    def _get_state(self):
+        if self.connection is None:
+            raise RuntimeError('Cannot get state while not connected to emulator.')
+
+        frame = self._receive_frame(self.connection)
+        observation = np.asarray(frame)
+        info = self._get_info(frame)
+
+        return observation, info
+
     def _receive_frame(self, connection: Connection) -> Image.Image:
         buffer = connection.socket.recv(const.BUFFER_SIZE, socket.MSG_WAITALL)
         return Image.frombytes('RGB', (const.BUFFER_WIDTH, const.BUFFER_HEIGHT), buffer)
 
     def _get_info(self, frame: Image.Image) -> dict[str, Any]:
+        p1_bar = ImageOps.mirror(frame.crop(const.P1_HP_BAR_REGION))
+        p2_bar = frame.crop(const.P2_HP_BAR_REGION)
+
+        p1_hp = self._calculate_hp(p1_bar)
+        p2_hp = self._calculate_hp(p2_bar)
+
         return {
-            'p1_health': 100.0,
-            'p2_health': 100.0
+            'agent_hp': p1_hp if self.is_player_one else p2_hp,
+            'opponent_hp': p2_hp if self.is_player_one else p1_hp
         }
+
+    def _calculate_hp(self, hp_bar: Image.Image) -> int:
+        hp = 1
+        reference = hp_bar.getpixel((0, 0))
+
+        if reference not in const.HP_COLORS:
+            return 0
+
+        for i in range(1, hp_bar.size[0]):
+            pixel = hp_bar.getpixel((i, 0))
+            
+            if pixel == reference:
+                hp += 1
+            else:
+                break
+
+        return hp
+
+    def _action_to_inputs(self, action):
+        inputs = np.zeros(29, dtype=np.int8)
+        
+        if self.is_player_one:
+            inputs[2:12] = action
+        else:
+            inputs[14:24] = action
+
+        return inputs
 
     def _close_connection(self):
         if self.connection is not None:
--- ai/src/third_strike_ai/main.py
+++ ai/src/third_strike_ai/main.py
@@ -1,46 +1,36 @@
 import socket
 import numpy as np
 from PIL import Image
-from third_strike_ai import constants as const
 
 import typer
 import gymnasium
 
 app = typer.Typer()
 
-def process_image(buffer: bytes) -> list[bool]:
-    im = Image.frombytes('RGB', (const.BUFFER_WIDTH, const.BUFFER_HEIGHT), buffer)
-    inputs = [False] * 29
-    inputs[5] = True
-    return inputs
-
-def loop():
-    sock = socket.socket()
-    sock.bind(('', const.PORT))
-    sock.listen(1)
-
-    connection, address = sock.accept()
-    print(f'Received a connection at address: {address}')
-
-    while True:
-        buffer = connection.recv(const.BUFFER_SIZE, socket.MSG_WAITALL)
-
-        if not buffer:
-            break
-
-        inputs = process_image(buffer)
-        connection.send(bytearray(inputs))
-
-    connection.close()
-
 @app.command()
 def run():    
     env = gymnasium.make(
         "ThirdStrike-v0", 
-        executable='./.build/Build/Products/Debug/FinalBurn Neo.app/Contents/MacOS/FinalBurn Neo'
+        executable='./.build/Build/Products/Debug/FinalBurn Neo.app/Contents/MacOS/FinalBurn Neo',
+        is_player_one=True
     )
 
     observation, info = env.reset()
-    image = Image.fromarray(observation)
-    image.show()
-    print(info)
+    opponent_hp = 160
+
+    while True:
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        new_opponent_hp = info['opponent_hp']
+
+        if new_opponent_hp < opponent_hp:
+            print(f'💥 {opponent_hp - new_opponent_hp} damage')
+            
+            if new_opponent_hp == 0:
+                print('💀 dead')
+
+        opponent_hp = new_opponent_hp
+
+        if terminated or truncated:
+            break

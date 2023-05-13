eeef43a28aebb5106d11a0fa58c4608d8b4dee63
Artem Pstygo <apstygo.dev@gmail.com>
2023-05-13 16:21:18 +0400

    restructure the python project

--- ai/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="third_strike_ai",
-    version="0.0.1",
-    install_requires=["gymnasium", "torch", "pillow", "numpy"],
-)
--- ai/src/third_strike_ai/main.py
+++ ai/src/third_strike_ai/main.py
@@ -1,29 +1,25 @@
 import socket
 import numpy as np
 from PIL import Image
+from third_strike_ai import constants as const
 
-PORT = 8080
-BUFFER_WIDTH = 384
-BUFFER_HEIGHT = 224
-BUFFER_BBP = 3
-BUFFER_SIZE = BUFFER_WIDTH * BUFFER_HEIGHT * BUFFER_BBP
 
 def process_image(buffer: bytes) -> list[bool]:
-    im = Image.frombytes('RGB', (BUFFER_WIDTH, BUFFER_HEIGHT), buffer)
+    im = Image.frombytes('RGB', (const.BUFFER_WIDTH, const.BUFFER_HEIGHT), buffer)
     inputs = [False] * 29
     inputs[5] = True
     return inputs
 
 def loop():
     sock = socket.socket()
-    sock.bind(('', PORT))
+    sock.bind(('', const.PORT))
     sock.listen(1)
 
     connection, address = sock.accept()
     print(f'Received a connection at address: {address}')
 
     while True:
-        buffer = connection.recv(BUFFER_SIZE, socket.MSG_WAITALL)
+        buffer = connection.recv(const.BUFFER_SIZE, socket.MSG_WAITALL)
 
         if not buffer:
             break
